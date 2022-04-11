4236b4548e5830ea03ebf8cd0d55e8efc4e65273
lharrak-botaina <lharrak.botaina@gmail.com>
2022-04-11 10:15:26 +0000

    organisation

--- admin/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-/* Define username and password */
-$Username = 'superuser';
-$Password = '_DPYuk7lxx7kNqKYe0lU';
--- config.php
+++
@@ -1,6 +0,0 @@
-<?php
-$dbhost="localhost";
-$dbuser="superuser";  
-$dbpass="p@ssword1"; 
-$dbname="productiondb"; 
-?>
