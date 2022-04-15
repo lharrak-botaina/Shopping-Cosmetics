3b603e571f55ef682c7bbd8a4e5a0caaebafea8d
tebbaa-adnane <razorzeroday@gmail.com>
2022-04-15 14:35:51 +0000

    add.

--- Challenges/challenge.1/gestion.php
+++ Challenges/challenge.1/gestion.php
@@ -13,17 +13,17 @@ class Gestion {
 
 
     public function getCartLine(){
-        $sql = "SELECT * ,SUM(Product_cart_quantity) FROM cartline INNER JOIN produit on produit.Id_product=cartline.id_product GROUP BY Id_Product ";
+        $sql = "SELECT * ,SUM(Product_cart_quantity) FROM cartline INNER JOIN products on products.id=cartline.Id_product GROUP BY Id_Product ";
         $query = mysqli_query($this->getConnection(), $sql);
         $result = mysqli_fetch_all($query, MYSQLI_ASSOC);
-        //
+        // 
         $cartLineList = array();
         foreach($result as $value){
             $product = new Product();
             $cartLine = new CartLine();
             $cartLine->setIdCartLine($value['id']);
             $cartLine->setIdCart($value['Id_cart']);
-            $cartLine->setIdProduct($value['id_Product']);
+            $cartLine->setIdProduct($value['Id_product']);
             $cartLine->setProductCartQuantity($value['SUM(Product_cart_quantity)']);
             $cartLine->setProduct($value['Name']);
             array_push($cartLineList, $cartLine);
