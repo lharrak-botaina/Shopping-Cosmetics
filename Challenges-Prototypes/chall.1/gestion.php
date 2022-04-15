0eaea9ec0fee6aeebcf0bbd132b51bf689d7193e
tebbaa-adnane <razorzeroday@gmail.com>
2022-04-15 12:17:53 +0000

    add chall.1

---
+++ Challenges-Prototypes/chall.1/gestion.php
@@ -0,0 +1,33 @@
+<?php
+include "cartLine.php";
+include "product.php";
+
+
+class Gestion {
+    public $name ;
+    private $Connection = Null;
+    private function getConnection(){
+            $this->Connection = mysqli_connect('localhost', 'superuser', 'p@ssword1', 'productiondb');
+        return $this->Connection;
+    }
+
+
+    public function getCartLine(){
+        $sql = "SELECT * ,SUM(Product_cart_quantity) FROM cartline INNER JOIN produit on produit.Id_product=cartline.id_product GROUP BY Id_Product ";
+        $query = mysqli_query($this->getConnection(), $sql);
+        $result = mysqli_fetch_all($query, MYSQLI_ASSOC);
+        //
+        $cartLineList = array();
+        foreach($result as $value){
+            $product = new Product();
+            $cartLine = new CartLine();
+            $cartLine->setIdCartLine($value['id']);
+            $cartLine->setIdCart($value['Id_cart']);
+            $cartLine->setIdProduct($value['id_Product']);
+            $cartLine->setProductCartQuantity($value['SUM(Product_cart_quantity)']);
+            $cartLine->setProduct($value['Name']);
+            array_push($cartLineList, $cartLine);
+        }
+        return $cartLineList;
+    }
+}
---
+++ Challenges-Prototypes/chall.1/index.php
@@ -0,0 +1,49 @@
+<?php 
+
+include "gestion.php";
+$gestion = new Gestion();
+
+
+$data = $gestion->getCartLine();
+ foreach($data as $value){ 
+    
+  echo  '<br>' .$value->getProductCartQuantity() .'<br>';
+  echo '<br>'. $value->getProduct();
+ $chart_data .= "{ Produit:'". $value->getProduct()."',numbre:".$value->getProductCartQuantity()."}, ";
+}
+$chart_data = substr($chart_data, 0, -2);
+?>
+
+
+<!DOCTYPE html>
+<html>
+ <head>
+  <title>chart with PHP & Mysql | lisenme.com </title>
+  <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.css">
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/raphael/2.1.0/raphael-min.js"></script>
+  <script src="//cdnjs.cloudflare.com/ajax/libs/morris.js/0.5.1/morris.min.js"></script>
+  
+ </head>
+ <body>
+  <br /><br />
+  <div class="container" style="width:900px;">
+
+   <h3 align="center">Total d'ajouter un produit au  panier</h3>   
+   <br /><br />
+   <div id="chart"></div>
+  </div>
+ </body>
+</html>
+
+<script>
+Morris.Bar({
+ element : 'chart',
+ data:[<?php echo $chart_data; ?>],
+ xkey:'Produit',
+ ykeys:['numbre', ],
+ labels:['numbre'],
+ hideHover:'auto',
+ stacked:true
+});
+</script>
